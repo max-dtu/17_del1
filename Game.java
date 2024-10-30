2ac3638c9bb61bc15eb770c16e2137fa017280c1
truthpapers <abuabdallah@Abus-MacBook-Pro.local>
2024-10-30 19:18:57 +0100

    Fixed a mistake

--- Game.java
+++ Game.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 
 public class Game {
     
-    Account private player1Account = new Account();
-    Account private player2Account = new Account();
-    int private playBalanceOfPlayer1 = player1Account.withdraw(1000);
-    int private playBalanceOfPlayer2 = player2Account.withdraw(1000);
+     private Account player1Account = new Account();
+     private Account player2Account = new Account();
+     private int playBalanceOfPlayer1 = player1Account.withdraw(1000);
+     private int playBalanceOfPlayer2 = player2Account.withdraw(1000);
 
     //The first argument is the playe's number (id), the second is his balance
     private Player player1 = new Player(1, playBalanceOfPlayer1);  
